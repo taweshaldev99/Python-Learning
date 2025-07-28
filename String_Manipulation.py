aaa8c78e02ca7bf15287414552c5abbd675aa9fa
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2025-07-28 23:44:29 +0545

    Add files via upload

---
+++ String_Manipulation.py
@@ -0,0 +1,23 @@
+a=2
+b=4
+
+print(a**b)
+txt="Nimisha GUPpta"
+print(txt)
+print(txt.upper())
+print(txt.lower())
+print(txt.title())
+
+word= "  This code is for Codewar Practice . here we learn python basic."
+print(word.split())  #['This', 'is', 'our', '2nd', 'day.', 'here', 'we', 'learn', 'python', 'basic.']
+print(word.split('.')) #['This code is for Codewar Practice ', ' here we learn python basic']
+print(word.splitlines())
+print(word.replace('i','I'))
+print(len(word))
+print(word.strip())
+
+a= 2
+b=4
+c=a+b
+print(c)
+print(f'the result is {c}')

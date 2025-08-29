fba0441e23a929a56bd11b73de384e798c19a4ac
Taweshal Dev Thakur <143280866+taweshaldev99@users.noreply.github.com>
2025-08-29 09:15:59 +0545

    Add files via upload

---
+++ NumPyWorks/Array-01.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr= np.array([1,2,3,4,5])
+name= np.array(["Taweshal","Dev","Thakur","Jasmine","Aara","Hussain"])
+print(arr)
+print(type(arr))
+print(arr[0]) # Array Indexing
+print(name[3])
